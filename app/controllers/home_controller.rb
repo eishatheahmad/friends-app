f9142e740578d5fb97e103cccfafe6c9b61956e6
Eisha Ahmad <eishaahmad16@gmail.com>
2021-08-09 13:33:17 +0500

    First Commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+    
+  end
+
+  def about
+    @about_me="This is a friends app made by using Ruby on Rails to demonstrate the basic CRUD funcions"
+
+  end
+  
+end
